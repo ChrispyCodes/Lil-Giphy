2a3a4e6375b514a5f7ec36a77c3ab9daab9cd810
Christopher <thetoph@outlook.com>
2018-08-31 14:30:34 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//
+function getGif() {
+  let input = $("#searchtext").val();
+  var xhr = $.get(
+    "http://api.giphy.com/v1/gifs/search?q=" +
+      input +
+      "+&api_key=QvMX1GF1Gy8qZJiUsDL9yClhF7Xm1djW&limit=30"
+  );
+  xhr.done(function(response) {
+    console.log("success got data", response);
+
+    let gifs = response.data;
+
+    for (i in gifs) {
+      $(".inner").append(
+        "<img src='" +
+          gifs[i].images.original.url +
+          "' style='height: 350px; width: 350px;'/>"
+      );
+    }
+  });
+  return;
+}
