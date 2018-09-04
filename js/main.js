3d01eb32372646fe20d3986377c8c27671d41322
Christopher <thetoph@outlook.com>
2018-09-04 15:48:44 -0700

    adding styles to search bar

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function getGif() {
   var xhr = $.get(
     "http://api.giphy.com/v1/gifs/search?q=" +
       input +
-      "+&api_key=QvMX1GF1Gy8qZJiUsDL9yClhF7Xm1djW&limit=30"
+      "+&api_key=QvMX1GF1Gy8qZJiUsDL9yClhF7Xm1djW&limit=10"
   );
   xhr.done(function(response) {
     console.log("success got data", response);
